74bfb1086294fb31d86385d049cc2dd914f06044
davutyaman <yamandavut@gmail.com>
2017-12-25 15:18:49 +0300

    components created and youtube api added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyDjbtUYh0N5ieHPqea0IHRXRD4fV8jEtPw';
+
 // Create a new component. This component should produce some HTML
-const App = function() {
+const App = () => {
     return <div>https://www.udemy.com/react-redux/learn/v4/t/lecture/4288046?start=0</div>;
 }
 
 //Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
