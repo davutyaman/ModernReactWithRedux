cd2b0d84e133b68411a4bcc64f1ee9d3ef9e2260
davutyaman <yamandavut@gmail.com>
2017-12-26 14:59:18 +0300

    search bar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyDjbtUYh0N5ieHPqea0IHRXRD4fV8jEtPw';
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-    return <div>https://www.udemy.com/react-redux/learn/v4/t/lecture/4288046?start=0</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 //Take this component's generated HTML and put it on the page (in the DOM)
